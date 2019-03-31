5c0838645e4218952b1a99d0dce6001577fd93d8
agatha12 <agkiecun@gmail.com>
2019-03-31 12:57:47 -0400

    skills in layout

--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -1,9 +1,26 @@
 import React from 'react'
 import './skills.css'
+import { Col, Row, CardPanel } from 'react-materialize'
 
 const Skills = () => {
     return (
         <div id="skills">
+         <Row>
+                <br></br>
+                <h2>Skills</h2>
+                <br></br>
+            </Row>
+            <Row>
+                <Col s={12}>
+                <CardPanel>
+                <i className="skill material-icons md-48">account_circle</i>
+                <i className="skill material-icons md-48">account_circle</i>
+                <i className="skill material-icons md-48">account_circle</i>
+                <i className="skill material-icons md-48">account_circle</i>
+                <i className="skill material-icons md-48">account_circle</i>
+                </CardPanel>
+                </Col>
+            </Row>
         
         </div>
 
