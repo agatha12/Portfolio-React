7d03efe586900e0046eabe97404b6d196eba4466
agatha12 <agkiecun@gmail.com>
2019-04-03 20:28:21 -0400

    mobile

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -14,14 +14,14 @@ const About = () => {
             </Row>
             <Row >
             
-                <Col s={6}>
+                <Col m={6} s={12}>
                 <div>
                     <br></br>
                 <img className="animated slideInLeft" id="agpic" src={agpic} alt="Agatha Kiecun"></img>
                 <p className="animated slideInLeft" id="shedPlug">Shedrick Pelt <a target="blank" href="https://www.sdotpdotmedia.com">sdotpdotmedia.com</a> social media: @sdotpdotmedia</p>
                 </div>
                 </Col>
-                <Col s={5}>
+                <Col m={5} s={12}>
                 <CardPanel className="cyan darken-4 animated slideInRight">
                         
                         <p>Enthusiastic full stack developer with a background in Quality Control and Management. Recent graduate with certificated from Rutgers University 24-week-long intensive boot camp. A highly motivated, fast learner eager to expand my knowledge of the field and contribute in a meaningful way. Great communicator with experience collaborating on a team to creatively solve problems and produce a quality product on a tight deadline. Detail oriented programmer who thrives in a fast-paced environment. Proficient in multiple programming languages and highly adept at learning new technologies. </p>
